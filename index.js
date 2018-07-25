51283bfec279a8bfbdb839827ba11be9a9e9a6b7
Jon Scheiding <jscheiding@certicasolutions.com>
2018-07-25 11:21:40 -0400

    Constants and Cleaner Code™

--- index.js
+++ index.js
@@ -3,32 +3,30 @@ import 'dotenv/config';
 import requestPromise from 'request-promise';
 
 const { JIRA_USERNAME, JIRA_API_TOKEN, JIRA_BASE_URL } = process.env;
+const STORY_POINTS_FIELD = 'customfield_10005';
+const EPIC_LINK_FIELD = 'customfield_10009';
+const TASK_ISSUETYPE_ID = 10002;
+const CAUSES_LINK_TYPE_ID = 10300;
 
 const request = requestPromise.defaults({
   auth: {
     user: JIRA_USERNAME,
     pass: JIRA_API_TOKEN
   },
-  baseUrl: JIRA_BASE_URL,
-  headers: [
-    { name: 'content-type', value: 'application/json' }
-  ],
+  baseUrl: URL.resolve(JIRA_BASE_URL, '/rest/api/2/').toString(),
   json: true
 });
 
-const STORY_POINTS = 'customfield_10005';
-const EPIC_LINK = 'customfield_10009';
-
-const createImplementationTask = async issueKey => {
+const createImplementationTaskForStory = async issueKey => {
   const story = await request(`issue/${issueKey}`);
   if(story.fields.issuetype.name !== 'Story') {
     throw new Error(`Issue ${issueKey} is a ${story.fields.issueType.name}, expected it to be a Story.`);
   }
 
-  const createBody = {
+  const createTaskRequestBody = {
     fields: {
       issuetype: {
-        id: 10002 // TASK
+        id: TASK_ISSUETYPE_ID
       },
       project: {
         id: story.fields.project.id
@@ -36,39 +34,31 @@ const createImplementationTask = async issueKey => {
       summary: story.fields.summary,
       description: `Implementation task for ${issueKey}`,
       fixVersions: story.fields.fixVersions.map(fixVersion => ({id: fixVersion.id})),
-      [EPIC_LINK]: story.fields[EPIC_LINK],
-      [STORY_POINTS]: story.fields[STORY_POINTS],
+      [EPIC_LINK_FIELD]: story.fields[EPIC_LINK_FIELD],
+      [STORY_POINTS_FIELD]: story.fields[STORY_POINTS_FIELD],
     }
   };
 
-  const task = await request('issue', {
-    method: 'POST',
-    body: createBody
-  });
+  const createdTask = await request('issue', 
+    { method: 'POST', body: createTaskRequestBody });
 
-  await request('issueLink', {
-    method: 'POST',
-    body: {
-      type: {
-        id: 10300 // CAUSES / IS CAUSED BY
-      },
-      inwardIssue: {
-        id: story.id
-      },
-      outwardIssue: {
-        id: task.id
-      }
-    }
-  });
+  const createIssueLinkRequestBody = {
+    inwardIssue: { id: story.id },
+    type: { id: CAUSES_LINK_TYPE_ID },
+    outwardIssue: { id: createdTask.id }  
+  };
 
-  return task.key;
+  await request('issueLink', 
+    { method: 'POST', body: createIssueLinkRequestBody });
+
+  return createdTask.key;
 };
 
-const createImplementationTasks = async storyKeys => {
+const createImplementationTasksForStories = async storyKeys => {
   for (const storyKey of storyKeys) {
-    const taskKey = await createImplementationTask(storyKey);
+    const taskKey = await createImplementationTaskForStory(storyKey);
     console.log(`Created task ${taskKey} for implementation of story ${storyKey}.`);
   }
 };
 
-createImplementationTasks(process.argv.slice(2));
+createImplementationTasksForStories(process.argv.slice(2));
